60fe539070b977aa24d780e991bcb189022bdf31
sabruss <sabruss@users.noreply.github.com>
2021-03-13 15:23:57 +0100

    Simple bot clean channel musique

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async message => {
+    if (!(message.guild.id === '819276769693401099' && message.channel.id === '819276770343387150')) {
+        return;
+    }
+
+    if (message.deletable) {
+        console.log(`Message to delete: [${message.id}] from ${message.author.tag}`)
+        try {
+            await message.delete({
+                timeout: 10000,
+                reason: 'Clean musique channel'
+            })
+            console.log(`Message deleted: [${message.id}] from ${message.author.tag}`)
+        } catch (e) {
+            console.error(e)
+        }
+
+    }
+
+});
+
+
+// client.login(process.env.BOT_TOKEN);
+client.login("NzgyMzIxNzQ2ODI0MDAzNjA1.X8Kf5w.JdDXLarhxGVOsUtrF5SPJFX4_rY");
