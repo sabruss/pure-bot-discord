3a2222ee9f7f541a44303b10646ba18573c9e191
sabruss <sabruss@users.noreply.github.com>
2021-03-13 17:50:55 +0100

    Fix send dm to bot Attempt 2

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ client.on('ready', () => {
 });
 
 client.on('message', async message => {
-    if (!(message.guild.id === '819276769693401099' && message.channel.type === 'text' && message.channel.id === '819276770343387150')) {
+
+    if (!(message.channel.type === 'text')) {
+        return;
+    }
+
+    if (!(message.guild.id === '819276769693401099' && message.channel.id === '819276770343387150')) {
         return;
     }
 
@@ -26,4 +31,8 @@ client.on('message', async message => {
     }
 });
 
-client.login(process.env.BOT_TOKEN);
+try {
+    await client.login(process.env.BOT_TOKEN || '');
+} catch (e) {
+    console.error(e)
+}
